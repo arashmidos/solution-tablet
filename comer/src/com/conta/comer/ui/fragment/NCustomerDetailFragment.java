3c9b54da566d46fb57d2fe630984f004fc76c075
Arashmidos <arashmidos2006@gmail.com>
2017-02-06 13:04:43 +0330

    make postal code mandatory for new customers

--- comer/src/com/conta/comer/ui/fragment/NCustomerDetailFragment.java
+++ comer/src/com/conta/comer/ui/fragment/NCustomerDetailFragment.java
@@ -352,6 +352,7 @@ public class NCustomerDetailFragment extends BaseContaFragment
             customer.setNationalCode(nationalCodeTxt.getText().toString());
             customer.setMunicipalityCode(municipalityCodeTxt.getText().toString());
             customer.setPostalCode(postalCodeTxt.getText().toString());
+            customer.setApproved(false);
 
             if (validate())
             {
@@ -418,6 +419,20 @@ public class NCustomerDetailFragment extends BaseContaFragment
             return false;
         }
 
+        if (Empty.isEmpty(customer.getPostalCode()))
+        {
+            toastMessage(R.string.message_postal_code_is_required);
+            postalCodeTxt.requestFocus();
+            return false;
+        }
+
+        if (customer.getPostalCode().length() != 10)
+        {
+            toastMessage(R.string.message_postal_code_is_not_valid);
+            postalCodeTxt.requestFocus();
+            return false;
+        }
+
         String nationalCode = customer.getNationalCode();
         if (!Empty.isEmpty(nationalCode) && (!isValidNationalCode(nationalCode)))
         {
@@ -437,26 +452,26 @@ public class NCustomerDetailFragment extends BaseContaFragment
         } else
         {
             //Check for equal numbers
-            String[] allDigitEqual = {"0000000000","1111111111","2222222222","3333333333",
-                    "4444444444","5555555555","6666666666","7777777777","8888888888","9999999999"};
-           if( Arrays.asList(allDigitEqual).contains(nationalCode))
-           {
-               return false;
-           }else
-           {
-               int sum = 0;
-               int lenght = 10;
-               for( int i = 0; i < lenght-1;i++)
-               {
-                   sum += Integer.parseInt(String.valueOf(nationalCode.charAt(i))) * (lenght-i) ;
-               }
+            String[] allDigitEqual = {"0000000000", "1111111111", "2222222222", "3333333333",
+                    "4444444444", "5555555555", "6666666666", "7777777777", "8888888888", "9999999999"};
+            if (Arrays.asList(allDigitEqual).contains(nationalCode))
+            {
+                return false;
+            } else
+            {
+                int sum = 0;
+                int lenght = 10;
+                for (int i = 0; i < lenght - 1; i++)
+                {
+                    sum += Integer.parseInt(String.valueOf(nationalCode.charAt(i))) * (lenght - i);
+                }
 
-               int r = Integer.parseInt(String.valueOf(nationalCode.charAt(9)));
+                int r = Integer.parseInt(String.valueOf(nationalCode.charAt(9)));
 
-               int c = sum%11;
+                int c = sum % 11;
 
-               return (((c < 2) && (r == c)) || ((c >= 2) && ((11 - c) == r)));
-           }
+                return (((c < 2) && (r == c)) || ((c >= 2) && ((11 - c) == r)));
+            }
 
         }
     }
