9e75ddfa99c14ed0ef9dceddd0ec5f3919af908b
Arashmidos <arashmidos2006@gmail.com>
2017-02-11 14:21:42 +0330

    Remove ACRA

--- comer/src/com/conta/comer/CommerApplication.java
+++ comer/src/com/conta/comer/CommerApplication.java
@@ -2,21 +2,16 @@ package com.conta.comer;
 
 import android.app.Application;
 
-import com.conta.comer.util.BugSender;
 import com.conta.comer.util.TypefaceUtil;
-
 import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
 
 /**
  * Created by Mahyar on 7/8/2015.
  */
-@ReportsCrashes(formUri = "")
 public class CommerApplication extends Application
 {
-
     @Override
     public void onCreate()
     {
@@ -25,19 +20,9 @@ public class CommerApplication extends Application
         try
         {
             TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/IRANSansMobile.ttf");
-            initACRABugReporter();
         } catch (Exception e)
         {
             e.printStackTrace();
         }
     }
-
-    private void initACRABugReporter()
-    {
-        ACRA.init(this);
-        BugSender bugSender = new BugSender();
-        bugSender.setContext(this);
-        ACRA.getErrorReporter().setReportSender(bugSender);
-    }
-
 }
