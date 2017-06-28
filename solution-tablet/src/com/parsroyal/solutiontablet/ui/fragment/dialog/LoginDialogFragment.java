7952fc57aaf37aa37f5ac3a68539feb564c4cd34
arash <arashmidos2006@gmail.com>
2017-06-28 12:39:05 +0430

    card-136: show errors in setting's login inline

--- solution-tablet/src/com/parsroyal/solutiontablet/ui/fragment/dialog/LoginDialogFragment.java
+++ solution-tablet/src/com/parsroyal/solutiontablet/ui/fragment/dialog/LoginDialogFragment.java
@@ -2,29 +2,36 @@ package com.parsroyal.solutiontablet.ui.fragment.dialog;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import com.crashlytics.android.Crashlytics;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.parsroyal.solutiontablet.R;
-import com.parsroyal.solutiontablet.exception.UnknownSystemException;
 import com.parsroyal.solutiontablet.service.SettingService;
 import com.parsroyal.solutiontablet.service.impl.SettingServiceImpl;
 import com.parsroyal.solutiontablet.ui.MainActivity;
 import com.parsroyal.solutiontablet.util.Analytics;
 import com.parsroyal.solutiontablet.util.Empty;
-import com.parsroyal.solutiontablet.util.ToastUtil;
 import com.parsroyal.solutiontablet.util.constants.ApplicationKeys;
 
 /**
  * Created by mahyar on 12/14/15.
  */
-public class LoginDialogFragment extends DialogFragment {
+public class LoginDialogFragment extends DialogFragment implements TextWatcher {
 
   public static final String TAG = LoginDialogFragment.class.getSimpleName();
+  @BindView(R.id.usernameTxt)
+  EditText usernameTxt;
+  @BindView(R.id.passwordTxt)
+  EditText passwordTxt;
+  @BindView(R.id.error_msg)
+  TextView errorMsg;
 
   private SettingService settingService;
 
@@ -35,58 +42,75 @@ public class LoginDialogFragment extends DialogFragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    try {
-      settingService = new SettingServiceImpl(getActivity());
 
-      View view = inflater.inflate(R.layout.dialog_setting_login, null);
-      final EditText usernameTxt = (EditText) view.findViewById(R.id.usernameTxt);
-      final EditText passwordTxt = (EditText) view.findViewById(R.id.passwordTxt);
-      Button loginBtn = (Button) view.findViewById(R.id.loginBtn);
-      Button cancelBtn = (Button) view.findViewById(R.id.cancelBtn);
+    settingService = new SettingServiceImpl(getActivity());
 
-      loginBtn.setOnClickListener(view12 -> {
+    View view = inflater.inflate(R.layout.dialog_setting_login, null);
+    ButterKnife.bind(this, view);
 
-        String username = settingService.getSettingValue(ApplicationKeys.SETTING_USERNAME);
-        String password = settingService.getSettingValue(ApplicationKeys.SETTING_PASSWORD);
+    usernameTxt.addTextChangedListener(this);
+    passwordTxt.addTextChangedListener(this);
+    getDialog().setTitle(R.string.login_to_setting);
 
-        String usernameValue = usernameTxt.getText().toString();
-        String passwordValue = passwordTxt.getText().toString();
+    return view;
+  }
 
-        if (Empty.isEmpty(usernameValue)) {
-          ToastUtil.toastError(getActivity(), R.string.error_username_is_empty);
-          usernameTxt.requestFocus();
-          return;
-        }
-
-        if (Empty.isEmpty(passwordTxt.getText().toString())) {
-          ToastUtil.toastError(getActivity(), R.string.error_password_is_empty);
-          passwordTxt.requestFocus();
-          return;
-        }
-
-        if (username.equals(usernameValue) && password.equals(passwordValue)) {
-          Analytics.logLogin(true);
-          ((MainActivity) getActivity()).changeFragment(MainActivity.SETTING_FRAGMENT_ID, false);
-          LoginDialogFragment.this.dismiss();
-        } else {
-          ToastUtil.toastError(getActivity(), R.string.error_invalid_login);
-          Analytics.logLogin(false);
-        }
-
-      });
-
-      cancelBtn.setOnClickListener(view1 -> LoginDialogFragment.this.dismiss());
-
-      getDialog().setTitle(R.string.login_to_setting);
-
-      return view;
-
-    } catch (Exception e) {
-      Crashlytics.log(Log.ERROR, "Login Exception", "Error in settings login " + e.getMessage());
-
-      ToastUtil.toastError(getActivity(), new UnknownSystemException(e));
-      Log.e(TAG, e.getMessage(), e);
-      return inflater.inflate(R.layout.view_error_page, null);
+  @OnClick({R.id.loginBtn, R.id.cancelBtn})
+  public void onViewClicked(View view) {
+    switch (view.getId()) {
+      case R.id.loginBtn:
+        doLogin();
+        break;
+      case R.id.cancelBtn:
+        LoginDialogFragment.this.dismiss();
+        break;
     }
+  }
+
+  private void doLogin() {
+    String usernameValue = usernameTxt.getText().toString();
+    String passwordValue = passwordTxt.getText().toString();
+
+    if (Empty.isEmpty(usernameValue)) {
+      errorMsg.setText(R.string.error_username_is_empty);
+      errorMsg.setVisibility(View.VISIBLE);
+      usernameTxt.requestFocus();
+      return;
+    }
+
+    if (Empty.isEmpty(passwordValue)) {
+      errorMsg.setText(R.string.error_password_is_empty);
+      errorMsg.setVisibility(View.VISIBLE);
+      passwordTxt.requestFocus();
+      return;
+    }
+
+    String username = settingService.getSettingValue(ApplicationKeys.SETTING_USERNAME);
+    String password = settingService.getSettingValue(ApplicationKeys.SETTING_PASSWORD);
+
+    if (username.equals(usernameValue) && password.equals(passwordValue)) {
+      Analytics.logLogin(true);
+      ((MainActivity) getActivity()).changeFragment(MainActivity.SETTING_FRAGMENT_ID, false);
+      LoginDialogFragment.this.dismiss();
+    } else {
+      errorMsg.setText(R.string.error_invalid_login);
+      errorMsg.setVisibility(View.VISIBLE);
+      Analytics.logLogin(false);
+    }
+  }
+
+  @Override
+  public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+  }
+
+  @Override
+  public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+  }
+
+  @Override
+  public void afterTextChanged(Editable s) {
+    errorMsg.setVisibility(View.GONE);
   }
 }
