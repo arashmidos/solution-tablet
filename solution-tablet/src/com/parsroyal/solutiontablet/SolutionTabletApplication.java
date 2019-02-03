3645fee70d870cb20a5d9aecde51a4556263dbd6
ShakibHabibi <shkbhbb@gmail.com>
2019-02-03 03:47:40 +0330

    custom push

--- solution-tablet/src/com/parsroyal/solutiontablet/SolutionTabletApplication.java
+++ solution-tablet/src/com/parsroyal/solutiontablet/SolutionTabletApplication.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatDelegate;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
+
 import com.crashlytics.android.Crashlytics;
 import com.github.anrwatchdog.ANRWatchDog;
 import com.instacart.library.truetime.TrueTimeRx;
@@ -23,16 +24,20 @@ import com.parsroyal.solutiontablet.constants.Constants;
 import com.parsroyal.solutiontablet.data.entity.Position;
 import com.parsroyal.solutiontablet.util.Empty;
 import com.parsroyal.solutiontablet.util.PreferenceHelper;
+
+import co.ronash.pushe.Pushe;
 import io.fabric.sdk.android.Fabric;
 import io.github.inflationx.calligraphy3.CalligraphyConfig.Builder;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
 import io.reactivex.schedulers.Schedulers;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
+
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
@@ -41,152 +46,152 @@ import timber.log.Timber.DebugTree;
  */
 public class SolutionTabletApplication extends MultiDexApplication {
 
-  public static SolutionTabletApplication sInstance;
+    public static SolutionTabletApplication sInstance;
 
-  public static SharedPreferences sPreference;
-  private ArrayList<String> authorities = new ArrayList<>();
-  private Location lastKnownLocation;
-  private Position lastSavedPosition;
+    public static SharedPreferences sPreference;
+    private ArrayList<String> authorities = new ArrayList<>();
+    private Location lastKnownLocation;
+    private Position lastSavedPosition;
 
-  public static SolutionTabletApplication getInstance() {
-    return sInstance;
-  }
-
-  public static SharedPreferences getPreference() {
-    if (sPreference == null) {
-      sPreference = PreferenceManager.getDefaultSharedPreferences(
-          sInstance.getApplicationContext());
+    public static SolutionTabletApplication getInstance() {
+        return sInstance;
     }
 
-    return sPreference;
-  }
+    public static SharedPreferences getPreference() {
+        if (sPreference == null) {
+            sPreference = PreferenceManager.getDefaultSharedPreferences(
+                    sInstance.getApplicationContext());
+        }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    sInstance = this;
-    Fabric.with(this, new Crashlytics());
-    if (!BuildConfig.DEBUG) {
-    } else {
-      Timber.plant(new DebugTree());
+        return sPreference;
     }
 
-    new ANRWatchDog(10_000).setANRListener(error -> {
-      Timber.e(error);
-      Crashlytics.log(error.toString());
-      Crashlytics.logException(error);
-    }).setIgnoreDebugger(true).start();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        Fabric.with(this, new Crashlytics());
+        if (!BuildConfig.DEBUG) {
+        } else {
+            Timber.plant(new DebugTree());
+        }
+
+        new ANRWatchDog(10_000).setANRListener(error -> {
+            Timber.e(error);
+            Crashlytics.log(error.toString());
+            Crashlytics.logException(error);
+        }).setIgnoreDebugger(true).start();
 //    Pushe.initialize(this, true);
-    MultiDex.install(this);
+        MultiDex.install(this);
 
-    ViewPump.init(ViewPump.builder()
-        .addInterceptor(new CalligraphyInterceptor(new
-            Builder()
-            .setDefaultFontPath("fonts/IRANSansMobile.ttf")
-            .setFontAttrId(R.attr.fontPath)
-            .build()))
-        .build());
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(new
+                        Builder()
+                        .setDefaultFontPath("fonts/IRANSansMobile.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()))
+                .build());
 
-    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
-    setLanguage();
+        setLanguage();
 
-    reSyncTrueTime();
+        reSyncTrueTime();
 
-    Timber.d(getInstanceId());
-  }
+        Timber.d(getInstanceId());
+    }
 
-  @SuppressLint("CheckResult")
-  public void reSyncTrueTime() {
+    @SuppressLint("CheckResult")
+    public void reSyncTrueTime() {
 
 //    new Thread(() -> {
-    try {
+        try {
 
-      TrueTimeRx.build().withSharedPreferencesCache(this).withServerResponseDelayMax(2000).withConnectionTimeout(60_000)
-          .initializeRx("time.apple.com")
-          .subscribeOn(Schedulers.io())
-          .subscribe(date -> {
-            Timber.d("TrueTime was initialized and we have a time:%s ", date);
-          }, throwable -> {
-          });
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      Timber.e("Network time not initialized");
-    }
+            TrueTimeRx.build().withSharedPreferencesCache(this).withServerResponseDelayMax(2000).withConnectionTimeout(60_000)
+                    .initializeRx("time.apple.com")
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(date -> {
+                        Timber.d("TrueTime was initialized and we have a time:%s ", date);
+                    }, throwable -> {
+                    });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Timber.e("Network time not initialized");
+        }
 //    }).start();
-  }
-
-  public void setLanguage() {
-    Locale locale = new Locale(Constants.DEFAULT_LANGUAGE);
-
-    Resources res = getResources();
-    DisplayMetrics dm = res.getDisplayMetrics();
-    Configuration conf = res.getConfiguration();
-    conf.locale = locale;
-    res.updateConfiguration(conf, dm);
-  }
-
-  @SuppressLint("HardwareIds")
-  public String getInstanceId() {
-    TelephonyManager telephonyManager = (TelephonyManager) getSystemService(
-        Context.TELEPHONY_SERVICE);
-    if (ActivityCompat.checkSelfPermission(this, permission.READ_PHONE_STATE)
-        != PackageManager.PERMISSION_GRANTED) {
-      return "";
     }
 
-    return telephonyManager != null ? telephonyManager.getDeviceId() : "";
+    public void setLanguage() {
+        Locale locale = new Locale(Constants.DEFAULT_LANGUAGE);
+
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = locale;
+        res.updateConfiguration(conf, dm);
+    }
+
+    @SuppressLint("HardwareIds")
+    public String getInstanceId() {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(
+                Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            return "";
+        }
+
+        return telephonyManager != null ? telephonyManager.getDeviceId() : "";
 
 //    return Secure.getString(getContentResolver(), Secure.ANDROID_ID);
-  }
-
-  public void loadAuthorities() {
-    setAuthorities(PreferenceHelper.getAuthorities());
-  }
-
-  public void clearAuthorities() {
-    setAuthorities(new HashSet<>());
-  }
-
-  public Location getLastKnownLocation() {
-    return lastKnownLocation;
-  }
-
-  public void setLastKnownLocation(Location lastKnownLocation) {
-    this.lastKnownLocation = lastKnownLocation;
-  }
-
-  public Position getLastSavedPosition() {
-    return lastSavedPosition;
-  }
-
-  public void setLastSavedPosition(Position lastSavedPosition) {
-    this.lastSavedPosition = lastSavedPosition;
-  }
-
-  private void setAuthorities(Set<String> authorities) {
-    if (Empty.isNotEmpty(authorities)) {
-      this.authorities.clear();
-      this.authorities.addAll(authorities);
     }
-  }
 
-  public boolean hasAccess(Authority authority) {
-
-    return BuildConfig.DEBUG || authorities.contains(String.valueOf(authority.getId()));
-  }
-
-  public Date getTrueTime() {
-    try {
-      if (TrueTimeRx.isInitialized()) {
-        return TrueTimeRx.now();
-      } else {
-        reSyncTrueTime();
-      }
-    } catch (Exception ex) {
-      ex.printStackTrace();
+    public void loadAuthorities() {
+        setAuthorities(PreferenceHelper.getAuthorities());
     }
-    return new Date();
-  }
+
+    public void clearAuthorities() {
+        setAuthorities(new HashSet<>());
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(Location lastKnownLocation) {
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public Position getLastSavedPosition() {
+        return lastSavedPosition;
+    }
+
+    public void setLastSavedPosition(Position lastSavedPosition) {
+        this.lastSavedPosition = lastSavedPosition;
+    }
+
+    private void setAuthorities(Set<String> authorities) {
+        if (Empty.isNotEmpty(authorities)) {
+            this.authorities.clear();
+            this.authorities.addAll(authorities);
+        }
+    }
+
+    public boolean hasAccess(Authority authority) {
+
+        return BuildConfig.DEBUG || authorities.contains(String.valueOf(authority.getId()));
+    }
+
+    public Date getTrueTime() {
+        try {
+            if (TrueTimeRx.isInitialized()) {
+                return TrueTimeRx.now();
+            } else {
+                reSyncTrueTime();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new Date();
+    }
 
 }
